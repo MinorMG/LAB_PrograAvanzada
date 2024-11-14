99ee07b929b8548b5c4cd3c69cdea5ab5f6fac85
brandonUre2 <145625481+brandonUre2@users.noreply.github.com>
2024-11-14 17:03:31 -0600

    Add files via upload

---
+++ Script Lab.sql
@@ -0,0 +1,22 @@
+use LabG9
+
+
+INSERT INTO [LabG9].[dbo].[Categorias] ([NombreCategoria], [Descripcion])
+VALUES 
+    ('Frutas', 'Productos frescos y orgánicos como manzanas, plátanos y naranjas.'),
+    ('Verduras', 'Verduras frescas y naturales como lechuga, zanahorias y tomates.'),
+    ('Lácteos', 'Productos derivados de la leche como yogur, queso y leche.'),
+    ('Cereales', 'Cereales integrales y orgánicos como avena, arroz y quinoa.');
+
+	CREATE TRIGGER trg_DesactivarProductoes
+ON Productoes
+AFTER UPDATE
+AS
+BEGIN
+    IF UPDATE(cantidad)
+    BEGIN
+        UPDATE Productoes
+        SET Estado = 0
+        WHERE IdProducto IN (SELECT IdProducto FROM inserted WHERE Cantidad = 0)
+    END
+END
